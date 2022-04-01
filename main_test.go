2d9f523c389a7734f53f14efded6fb6ab05b4cf0
OhMimi <gash60535@gmail.com>
2022-04-01 15:04:00 +0800

    add parallel benchtest

--- main_test.go
+++ main_test.go
@@ -1,13 +1,15 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkGenerateBwmarrinSnowflake(b *testing.B) {
 
 	bw := NewBW()
 
 	for i := 0; i < b.N; i++ {
-		bw.Generate().Int64()
+		bw.Generate()
 	}
 }
 
@@ -18,4 +20,28 @@ func BenchmarkGenerateFishTennisSnowflake(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ft.NextId()
 	}
+}
+
+func BenchmarkGenerateBwmarrinSnowflakeParallel(b *testing.B) {
+
+	bw := NewBW()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			bw.Generate()
+		}
+	})
+
+}
+
+func BenchmarkGenerateFishTennisSnowflakeParallel(b *testing.B) {
+
+	ft := NewFt()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			ft.NextId()
+		}
+	})
+
 }
